f2532143251949a50d507c8b680042d4a4060aec
Filipe Verri <filipeverri@gmail.com>
2020-10-26 19:33:28 -0300

    More iterations, less networks.

--- exp.sh
+++ exp.sh
@@ -13,20 +13,20 @@ cmake --build build-release
 
 trap 'kill $(jobs -p); exit 1' SIGINT SIGTERM SIGKILL
 
-maxtime=200
-seed=17
-
+maxtime=500
 for lambda in {30..1}; do
-  filename="data/agents,d=35,35,5,150,s=$seed,l=$lambda.csv.gz"
-  if [ -f "$filename" ]; then
-    echo "Skipping s=$seed λ=$lambda"
-  else
-    nfilename="data/network,d=35,35,5,150,s=$seed,l=$lambda,t={time}.csv.gz"
-    parallel build-release/simulation -d 35 35 5 150 -s $seed -l $lambda \
-      -t $maxtime -p 0 \
-      -n "$nfilename" \
-      -o "$filename"
-  fi
+  for seed in {1..10}; do
+    filename="data/agents,d=35,35,5,150,s=$seed,l=$lambda.csv.gz"
+    if [ -f "$filename" ]; then
+      echo "Skipping s=$seed λ=$lambda"
+    else
+      nfilename="data/network,d=35,35,5,150,s=$seed,l=$lambda,t={time}.csv.gz"
+      parallel build-release/simulation -d 35 35 5 150 -s $seed -l $lambda \
+        -t $maxtime -p $((maxtime - 10)) \
+        -n "$nfilename" \
+        -o "$filename"
+    fi
+  done
 done
 
 wait
