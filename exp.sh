80d4d61df2c74b0e896ea07ed28f731c7c8f9f7c
Filipe Verri <filipeverri@gmail.com>
2020-10-27 09:08:14 -0300

    More seeds, less networks.

--- exp.sh
+++ exp.sh
@@ -15,14 +15,14 @@ trap 'kill $(jobs -p); exit 1' SIGINT SIGTERM SIGKILL
 
 maxtime=500
 for lambda in {30..1}; do
-  for seed in {1..10}; do
+  for seed in {1..20}; do
     filename="data/agents,d=35,35,5,150,s=$seed,l=$lambda.csv.gz"
     if [ -f "$filename" ]; then
       echo "Skipping s=$seed λ=$lambda"
     else
       nfilename="data/network,d=35,35,5,150,s=$seed,l=$lambda,t={time}.csv.gz"
       parallel build-release/simulation -d 35 35 5 150 -s $seed -l $lambda \
-        -t $maxtime -p $((maxtime - 10 + 1)) \
+        -t $maxtime -p $((maxtime - 5 + 1)) \
         -n "$nfilename" \
         -o "$filename"
     fi
